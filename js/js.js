ef8424cf1e9e1424f93dae8e070101ea4c624396
Bernardo Curso <bernardo-v.f@hotmail.com>
2022-08-23 23:36:54 -0300

    form

--- js/js.js
+++ js/js.js
@@ -1,8 +1,9 @@
 import dataHora from './modules/dataHora.js';
 import menuMobile from './modules/menu-mobile.js';
+import validarContato from './modules/validarContato.js';
 
 
 
-menuMobile()
-dataHora()
-
+validarContato()
+menuMobile();
+dataHora();
